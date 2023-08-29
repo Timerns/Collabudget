a77749b58f05a71fff0ba8cb8a3104c417e2ad0f
_Greenkiller_ <ericperonetti00@gmail.com>
2023-08-29 09:34:18 +0200

    Added dotenv + Required config to make it work

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -1,13 +1,14 @@
 import express, { Express, Request, Response } from 'express';
+import { config } from 'dotenv';
 
+config();
 const app = express();
 const port = 8000;
 
 app.listen(port, async () => {
-  
   app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Hello World ' + process.env.HELLO);
   });
   
-  console.log('🚀 We are live on http://localhost:' + port + '🚀');
+  console.log('🚀 We are live on http://localhost:' + port + ' 🚀');
 });
