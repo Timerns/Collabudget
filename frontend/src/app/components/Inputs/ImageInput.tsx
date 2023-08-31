7d6fec831c073928c91fd8eeb43f838e18cb4187
_Greenkiller_ <ericperonetti00@gmail.com>
2023-08-31 15:18:56 +0200

    Fixes + DropdownInput + DateInput

--- frontend/src/app/components/Inputs/ImageInput.tsx
+++ frontend/src/app/components/Inputs/ImageInput.tsx
@@ -23,7 +23,7 @@ const ImageInput = React.forwardRef<HTMLInputElement, ImageInputProps & ReturnTy
       <label className="block cursor-pointer shadow bg-white appearance-none border rounded w-full" htmlFor={props.name}>
         <div className="w-full items-center justify-center flex">
           <svg className="absolute w-10 h-10 text-gray-800 dark:text-dark stroke-light-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12V1m0 0L4 5m4-4 4 4m3 5v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"/>
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12V1m0 0L4 5m4-4 4 4m3 5v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"/>
           </svg>
           <img className="object-contain p-3 max-w-xxs" src={image} />
         </div>
