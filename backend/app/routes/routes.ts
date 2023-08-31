764f80a9219f9d927c4a46d718bb4ef5fb4d20c9
Killassa <greg12876@gmail.com>
2023-08-31 14:38:49 +0200

    Add a check to know if user is in group

--- backend/app/routes/routes.ts
+++ backend/app/routes/routes.ts
@@ -1,7 +1,22 @@
 import { Express } from 'express';
 import { Sequelize } from "sequelize";
 import { loginRoute } from './login_route';
+import { labelRoute } from './label_route';
+import { UserGroup } from '../database/userGroup';
 
 export function routes(app: Express, sequelize: Sequelize) {
   loginRoute(app, sequelize);
+  labelRoute(app, sequelize);
+}
+
+// Verify if the user is in the group to know if he is allowed to do the group's action
+export async function userInGroup(res: any, username?: string, groupId?: string): Promise<boolean> {
+  const inGroup = await UserGroup.findOne({ where: { UserUsername: username, GroupId: groupId } })
+
+  if (inGroup === null) {
+    res.json({ error: 'Vous ne faites pas parti de ce groupe !' })
+    return false
+  }
+
+  return true
 }
