66ecf39546f2128f977f74463dea7704510719d5
Killassa <greg12876@gmail.com>
2023-09-01 12:48:30 +0200

    Create routes for limit

--- backend/app/routes/routes.ts
+++ backend/app/routes/routes.ts
@@ -4,11 +4,13 @@ import { UserGroup } from '../database/userGroup';
 import { loginRoute } from './login_route';
 import { labelRoute } from './label_route';
 import { groupRoute } from './group_route';
+import { limitRoute } from './limit_route';
 
 export function routes(app: Express, sequelize: Sequelize) {
   loginRoute(app, sequelize);
   labelRoute(app, sequelize);
   groupRoute(app, sequelize);
+  limitRoute(app, sequelize);
 }
 
 // Verify if the user is in the group to know if he is allowed to do the group's action
