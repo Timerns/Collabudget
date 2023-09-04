1be2fe340a741633bb98913305578811e61be136
Tim Ernst <tim.ernst07.01@gmail.com>
2023-09-04 14:08:35 +0200

    dynamic groups

--- frontend/src/app/types/groupeType.tsx
+++ frontend/src/app/types/groupeType.tsx
@@ -1,17 +1,10 @@
 import LabelType from "@/app/types/labelType";
 
 export default class GroupeType {
-    id: number;
-    name: string;
-    currency: string;
-    inviteId: number;
-    labels: LabelType[];
-
-    constructor(id: number, name: string, currency: string, inviteId: number, labels: LabelType[]) {
-        this.id = id;
-        this.name = name;
-        this.currency = currency;
-        this.inviteId = inviteId;
-        this.labels = labels;
-    }
+  id!: number;
+  name!: string;
+  currency!: string;
+  inviteId!: string;
+  description!: string;
+  image!: string;
 }
