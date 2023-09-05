109165b00da615cfab44c8c86f71befa36982e11
Killassa <greg12876@gmail.com>
2023-09-05 22:59:22 +0200

    Add groupe update

--- frontend/src/app/app/groupe/[groupId]/page.tsx
+++ frontend/src/app/app/groupe/[groupId]/page.tsx
@@ -17,6 +17,7 @@ import { request } from "@/app/utils/database";
 import { toast } from "react-toastify";
 import SoldeType from "@/app/types/soldeType";
 import LabelTooltip from "@/app/components/labelTooltip";
+import UpdateGroupModal from "@/app/components/Modals/UpdateGroupe";
 
 export default function Page({params}: { params: { groupId: number } }) {
   type Data = {
@@ -26,6 +27,7 @@ export default function Page({params}: { params: { groupId: number } }) {
     soldes: SoldeType[]
   }
 
+  const [menu, setMenu] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   const [data, setData] = useState<Data>({
     group: {id: -1, currency: "", description: "", image: "", inviteId: "", name: "", soldes: []},
@@ -82,6 +84,10 @@ export default function Page({params}: { params: { groupId: number } }) {
     }
   }
 
+  function show() {
+    setMenu(!menu);
+  }
+
   return (
       <>
         <div className={"grid grid-cols-1 xl:grid-cols-3 gap-x-7"}>
@@ -100,7 +106,7 @@ export default function Page({params}: { params: { groupId: number } }) {
                 <svg className="inline mr-3 w-6 h-6 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="m13.835 7.578-.005.007-7.137 7.137 2.139 2.138 7.143-7.142-2.14-2.14Zm-10.696 3.59 2.139 2.14 7.138-7.137.007-.005-2.141-2.141-7.143 7.143Zm1.433 4.261L2 12.852.051 18.684a1 1 0 0 0 1.265 1.264L7.147 18l-2.575-2.571Zm14.249-14.25a4.03 4.03 0 0 0-5.693 0L11.7 2.611 17.389 8.3l1.432-1.432a4.029 4.029 0 0 0 0-5.689Z"/>
                 </svg>
-                Éditer
+                <UpdateGroupModal show={show} group={data.group} />
               </div>
               <div className={"col-span-1 text-primary hover:cursor-pointer hover:text-white"} onClick={copyToClipboard}>
                 <svg className="inline mr-3 w-6 h-6 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
@@ -121,6 +127,7 @@ export default function Page({params}: { params: { groupId: number } }) {
                 doubleRow={true} 
                 getInfo={(t) => (t.RefundedUsername === null ? t.UserUsername ?? "" : `De ${t.UserUsername} à ${t.RefundedUsername}`)}
                 getLabel={(t) => data.labels.find(x => x.id === Number(t.LabelId))}
+                group={data.group}
                 />
             }
           </div>
