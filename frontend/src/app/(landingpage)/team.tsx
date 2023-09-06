68693a206be512f035f4afdac9a2ca33ab8f874f
Killassa <greg12876@gmail.com>
2023-09-06 20:43:23 +0200

    Add images on landing page

--- frontend/src/app/(landingpage)/team.tsx
+++ frontend/src/app/(landingpage)/team.tsx
@@ -2,7 +2,7 @@ export default function Team() {
     return (
         <div className={"grid md:grid-cols-4"}>
             <div className={"col-span-1 mb-6"}>
-                <img src={"/web/user1.png"} className={"w-1/2 mx-auto mb-3"}/>
+                <img src={"/team/tim.jpg"} className={"w-48 h-48 object-cover mx-auto mb-3 rounded-full"}/>
                 <h3 className={"text-lg"}>Tim Ernst</h3>
                 <h3 className={"text-lg"}>Ingénierie des données</h3>
                 <h4 className={"text-sm"}>tim.ersnt@heig-vd.ch</h4>
@@ -14,7 +14,7 @@ export default function Team() {
                 <h4 className={"text-sm"}>eric.peronetti@heig-vd.ch</h4>
             </div>
             <div className={"col-span-1 mb-6"}>
-                <img src={"/web/user1.png"} className={"w-1/2 mx-auto mb-3"}/>
+                <img src={"/team/gregory.jpg"} className={"w-48 h-48 object-cover mx-auto mb-3 rounded-full"}/>
                 <h3 className={"text-lg"}>Grégory Rey-Mermet</h3>
                 <h3 className={"text-lg"}>Ingénierie des données</h3>
                 <h4 className={"text-sm"}>gregory.rey-mermet@heig-vd.ch</h4>
